668a444307e3617be3cc36023e44aeadf4579a68
prb01 <p.bergstroem@gmail.com>
2021-11-15 09:27:37 +0100

    Tile functions completed. Board skeleton started.

--- board.rb
+++ board.rb
@@ -1,4 +1,42 @@
 require_relative 'tile'
 
 class Board
+  def self.create_board(size, bombs)
+    board = []
+    board = Array.new(size) do
+      Array.new(size) { Tile.new(0, board) }
+    end
+    board = Board.populate_bombs(board, bombs)
+  end
+
+  def self.populate_bombs(board, bombs)
+    until bombs == 0
+      row = rand(0...board.count)
+      col = rand(0...board.count)
+      if board[row][col] != "B"
+        board[row][col].value = "B"
+        bombs -= 1
+      end
+    end
+    board
+  end
+
+  attr_reader :board
+
+  def initialize(board = Board.create_board(9, 10))
+    @board = Array.new(size) do
+      Array.new(size) { Tile.new(0, board) }
+    end
+  end
+
+  def [](pos)
+    row, col = pos
+    board[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    tile = board[row][col]
+    tile.value = value
+  end
 end
--- tile.rb
+++ tile.rb
@@ -1,4 +1,6 @@
 class Tile
+attr_reader :value, :flagged, :bombed, :revealed, :board
+
   def initialize(value, board)
     @value = value
     @board = board
@@ -11,4 +13,66 @@ class Tile
     { 'value' => @value, 'flagged' => @flagged, 
       'bombed' => @bombed, 'revealed' => @revealed }.inspect
   end
+
+  def value=(new_value)
+    @value = new_value
+  end
+
+  def toggle_reveal
+    @revealed = !@revealed
+  end
+
+  def my_pos
+    board.each_with_index do |rows, row|
+      rows.each_with_index do |tiles, col|
+        return [row, col] if self == tiles
+      end
+    end
+    nil
+  end
+
+  def my_row
+    row, col = my_pos
+    row
+  end
+
+  def my_col
+    row, col = my_pos
+    col
+  end
+
+  def neighbours
+    arr = []
+    (-1..1).each do |row|
+      (-1..1).each do |col|
+        next if row == 0 && col == 0
+
+        pos = [my_row + row, my_col + col]
+        new_row, new_col = pos
+        if valid_pos?(pos)
+          arr << board[new_row][new_col]
+        else
+          arr << nil
+        end
+      end
+    end
+    arr
+  end
+
+  def valid_pos?(pos)
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| x >= 0 && x < board.count }
+      return true
+    else
+      return false
+    end
+  end
+
+  def neighbour_bomb_count
+    neighbours.count { |tile| tile.value == "B" }
+  end
+
+  # private
+  # attr_reader :board
 end
