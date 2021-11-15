44a7f9b04ff26004af93e76a8d67eb48198a1d56
prb01 <p.bergstroem@gmail.com>
2021-11-15 09:35:40 +0100

    Updated board to create grid within initialize instead

--- board.rb
+++ board.rb
@@ -1,15 +1,36 @@
 require_relative 'tile'
 
 class Board
-  def self.create_board(size, bombs)
-    board = []
-    board = Array.new(size) do
-      Array.new(size) { Tile.new(0, board) }
+  # def self.create_board(size, bombs)
+  #   board = Array.new(size) { Array.new(size) }
+  #   board.each do |rows|
+  #   Tile.new(0, board)
+  #   board = Board.populate_bombs(board, bombs)
+  # end
+
+  # def self.populate_bombs(board, bombs)
+  #   until bombs == 0
+  #     row = rand(0...board.count)
+  #     col = rand(0...board.count)
+  #     if board[row][col] != "B"
+  #       board[row][col].value = "B"
+  #       bombs -= 1
+  #     end
+  #   end
+  #   board
+  # end
+
+  attr_reader :board
+
+  def initialize(size = 9)
+    @board = Array.new(size) { Array.new(size) }
+    @board.each do |rows|
+      rows.map! { |el| el = Tile.new(0, board) }
     end
-    board = Board.populate_bombs(board, bombs)
+    populate_bombs()
   end
 
-  def self.populate_bombs(board, bombs)
+  def populate_bombs(bombs = 10)
     until bombs == 0
       row = rand(0...board.count)
       col = rand(0...board.count)
@@ -17,15 +38,6 @@ class Board
         board[row][col].value = "B"
         bombs -= 1
       end
-    end
-    board
-  end
-
-  attr_reader :board
-
-  def initialize(board = Board.create_board(9, 10))
-    @board = Array.new(size) do
-      Array.new(size) { Tile.new(0, board) }
     end
   end
 
