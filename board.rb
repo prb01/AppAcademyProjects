c9c954b0447a0e9c8ef495757a88febf354c2558
prb01 <p.bergstroem@gmail.com>
2021-11-15 09:38:39 +0100

    Board now uses self methods to populate initial grid

--- board.rb
+++ board.rb
@@ -1,33 +1,30 @@
 require_relative 'tile'
 
 class Board
-  # def self.create_board(size, bombs)
-  #   board = Array.new(size) { Array.new(size) }
-  #   board.each do |rows|
-  #   Tile.new(0, board)
-  #   board = Board.populate_bombs(board, bombs)
-  # end
+  def self.create_board(size, bombs)
+    board = Array.new(size) { Array.new(size) }
+    board.each do |rows|
+      rows.map! { |tile| tile = Tile.new(0, board) }
+    end
+    board = Board.populate_bombs(board, bombs)
+  end
 
-  # def self.populate_bombs(board, bombs)
-  #   until bombs == 0
-  #     row = rand(0...board.count)
-  #     col = rand(0...board.count)
-  #     if board[row][col] != "B"
-  #       board[row][col].value = "B"
-  #       bombs -= 1
-  #     end
-  #   end
-  #   board
-  # end
+  def self.populate_bombs(board, bombs)
+    until bombs == 0
+      row = rand(0...board.count)
+      col = rand(0...board.count)
+      if board[row][col] != "B"
+        board[row][col].value = "B"
+        bombs -= 1
+      end
+    end
+    board
+  end
 
   attr_reader :board
 
-  def initialize(size = 9)
-    @board = Array.new(size) { Array.new(size) }
-    @board.each do |rows|
-      rows.map! { |el| el = Tile.new(0, board) }
-    end
-    populate_bombs()
+  def initialize(board = Board.create_board(9,10))
+    @board = board
   end
 
   def populate_bombs(bombs = 10)
