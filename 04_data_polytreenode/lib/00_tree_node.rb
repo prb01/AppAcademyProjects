4c0ea794dc5c528de3eb6abf60eecb9cbdc93bf9
prb01 <p.bergstroem@gmail.com>
2021-11-16 17:13:08 +0100

    Polytreenode moved to own folder

---
+++ 04_data_polytreenode/lib/00_tree_node.rb
@@ -0,0 +1,53 @@
+class PolyTreeNode
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def inspect
+    {'value' => value}.inspect
+    #, 'parent' => parent.value, 'children' => children.value
+  end
+
+  def parent=(parent_node)
+    parent.remove_child(self) if parent && parent_node
+    @parent = parent_node
+    parent.add_child(self) if parent
+  end
+
+  def add_child(child_node)
+    if children.none? { |child| child == child_node } 
+      children << child_node
+      child_node.parent = self if child_node.parent != self
+    end
+  end
+
+  def remove_child(child_node)
+    raise "Node is not a child" if !children.include?(child_node)
+    children.delete_if { |child| child == child_node }
+    child_node.parent = nil
+  end
+
+  def dfs(target)
+    return self if self.value == target
+
+    self.children.each do |child|
+      search_result = child.dfs(target)
+      return search_result if !search_result.nil?
+    end
+    nil
+  end
+
+  def bfs(target)
+    queue = [self]
+    until queue.empty?
+      el = queue.shift
+      return el if el.value == target
+      queue += el.children
+    end
+    nil
+  end
+end
