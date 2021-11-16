d51bde1965489dfee97caf345ab268592bb4ff20
prb01 <p.bergstroem@gmail.com>
2021-11-16 11:08:52 +0100

    Polytreenode class completed first pass

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,5 +1,29 @@
 class PolyTreeNode
-  def initialize
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
   end
-  
+
+  def parent=(new_parent)
+    parent.children.delete_if { |child| child == self } if parent
+    @parent = new_parent
+    parent.add_child(self) if parent
+  end
+
+  def add_child(new_child)
+    if children.none? { |child| child == new_child } 
+      children << new_child
+      new_child.parent = self if new_child.parent != self
+    end
+  end
+
+  def remove_child(old_child)
+    raise "Node is not a child" if !children.include?(old_child)
+    old_child.parent = nil
+    children.delete_if { |child| child == old_child }
+  end
+
 end
