6e38eb1e35fe1e58c3df949d1efcb303bdcc8cb9
prb01 <p.bergstroem@gmail.com>
2021-11-17 15:36:16 +0100

    Updated #move for Super AI to use nodes to prevent losing

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,18 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node_mark = ((mark == :x) ? :o : :x)
+    node = TicTacToeNode.new(game.board, mark)
+    winner = nil
+    non_loser = nil
+
+    node.children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark) && !child.losing_node?(mark)
+      non_loser = child.prev_move_pos if !child.losing_node?(mark)
+    end
+
+    raise "No non-losers. What the bejesus?" if non_loser.nil?
+    return non_loser
   end
 end
 
