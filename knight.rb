4485bda4aafdbd33e27747425954cb4349047240
prb01 <p.bergstroem@gmail.com>
2021-11-17 09:54:31 +0100

    Added #build_move_tree
    Creates all possible moves given starting position

--- knight.rb
+++ knight.rb
@@ -26,10 +26,38 @@ class KnightPathFinder
 
   def initialize(start_pos)
     @root_node = PolyTreeNode.new(start_pos)
+    @considered_positions = [start_pos]
     build_move_tree
   end
 
   def build_move_tree
+    queue = [self.root_node]
+    until queue.empty?
+      el = queue.shift
+      p el
+      p el.value
+      new_move_positions(el.value).each do |move|
+        child = PolyTreeNode.new(move)
+        child.parent = el
+      end
+      queue += el.children
+    end
+  end
+
+  #      queue = [self]
+  #   until queue.empty?
+  #     el = queue.shift
+  #     return el if el.value == target
+  #     queue += el.children
+  #   end
+  #   nil
+  # end
+
+  def new_move_positions(pos)
+    moves = KnightPathFinder.valid_moves(pos)
+    moves.reject! { |move| @considered_positions.include?(move) }
+    @considered_positions += moves
+    moves
   end
 
   def find_path
