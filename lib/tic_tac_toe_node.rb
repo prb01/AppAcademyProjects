b4bd5c503a3285a1e1d7374ec37d29986f1f4a2f
prb01 <p.bergstroem@gmail.com>
2021-11-17 14:25:08 +0100

    Added #losing_node? & #winning_node? | Updated #children
    \#losing_node?
    checks if node will lose given a mark
    
    \#winning_node?
    checks if node will win given a mark
    
    \#children
    updated method to put correct mark on dupe board & in child node

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,6 +1,8 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -8,13 +10,42 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    return false if board.over? && 
+      (board.tied? || board.winner == evaluator)
+    return true if board.over? && board.winner != evaluator
+
+    children.any? do |child|
+      child.losing_node?(evaluator)
+    end
   end
 
   def winning_node?(evaluator)
+    return true if board.over? && board.winner == evaluator
+    return false if board.over?
+    
+    children.any? do |child|
+      child.winning_node?(evaluator)
+    end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+    ln = board.rows.length
+
+    (0...ln).each do |r|
+      (0...ln).each do |c|
+        pos = [r,c]
+        if board.empty?(pos)
+          node_board = board.dup
+          node_board[pos] = next_mover_mark
+          node_next_mark = ((next_mover_mark == :x) ? :o : :x)
+          children << TicTacToeNode.new(node_board, node_next_mark, pos)
+        end
+      end
+    end
+
+    children
   end
 end
