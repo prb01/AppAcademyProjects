c73d0c7a2a27b03ab81a10b7fbc415278025d396
prb01 <p.bergstroem@gmail.com>
2021-11-23 15:00:05 +0100

    Board#initialize updated for all chess pieces

--- board.rb
+++ board.rb
@@ -5,17 +5,43 @@ class Board
 
   def initialize
     @rows = Array.new(8) { Array.new(8, nil) } 
-    # @null_piece = NullPiece.new()
+    @null_piece = NullPiece.instance
 
     @rows = @rows.each_with_index.map do |row, r|
       row.each_with_index.map do |el, c|
         case r
-        when 0, 1
-          Piece.new(:black, self, [r,c])
-        when 6, 7
-          Piece.new(:white, self, [r,c])
+        when 0
+          case c
+          when 0, 7
+            Rook.new(:black, self, [r,c])
+          when 1, 6
+            Knight.new(:black, self, [r,c])
+          when 2, 5
+            Bishop.new(:black, self, [r,c])
+          when 3
+            Queen.new(:black, self, [r,c])
+          when 4
+            King.new(:black, self, [r,c])
+          end
+        when 1
+          Pawn.new(:black, self, [r,c])
+        when 6
+          Pawn.new(:white, self, [r,c])
+        when 7
+          case c
+          when 0, 7
+            Rook.new(:white, self, [r,c])
+          when 1, 6
+            Knight.new(:white, self, [r,c])
+          when 2, 5
+            Bishop.new(:white, self, [r,c])
+          when 3
+            Queen.new(:white, self, [r,c])
+          when 4
+            King.new(:white, self, [r,c])
+          end
         else
-          nil
+          null_piece
         end
       end
     end
@@ -31,12 +57,16 @@ class Board
     rows[r][c] = val
   end
 
-  def move_piece(start_pos, end_pos)
-    raise "No piece at start position." if !self[start_pos]
-    raise "Cannot move to end position." if !end_pos.all? { |x| (0..7).include?(x) }
+  def move_piece(color, start_pos, end_pos)
+    if !valid_pos?(start_pos) || (self[start_pos] == null_piece)
+      raise "No piece at start position."
+    elsif !self[start_pos].moves.include?(end_pos)
+      raise "Cannot move to end position."
+    end
     
     self[end_pos] = self[start_pos]
-    self[start_pos] = nil
+    self[start_pos] = null_piece
+    self[end_pos].pos = end_pos
     true
   end
 
