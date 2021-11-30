45c96a9a0bd013fe490291821a337e7c7c786010
prb01 <p.bergstroem@gmail.com>
2021-11-30 14:59:19 +0200

    Display class minor changes to #render

--- display.rb
+++ display.rb
@@ -5,14 +5,15 @@ require_relative 'cursor'
 class Display
   attr_reader :board, :cursor
 
-  def initialize(board)
-    @board = Board.new
+  def initialize(board = Board.new)
+    @board = board
     @cursor = Cursor.new([0,0], board)
   end
 
   def render
     white_bg = true
     
+    system("clear")
     puts "   #{('a'..'h').to_a.join("  ")}  "
 
     board.rows.each_with_index do |rows, r|
@@ -34,7 +35,7 @@ class Display
         end
 
       end
-      
+
       puts "#{row_str} #{r}"
       white_bg = !white_bg
     end
@@ -42,3 +43,11 @@ class Display
     puts "   #{('a'..'h').to_a.join("  ")}  "
   end
 end
+
+# if __FILE__ == $PROGRAM_NAME
+#   display = Display.new
+#   while true
+#     display.render
+#     display.cursor.get_input
+#   end
+# end
