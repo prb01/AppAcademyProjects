e86e1b32aa875dd4c7e95bf155969b2812386492
prb01 <p.bergstroem@gmail.com>
2021-12-03 14:36:11 +0200

    Card class specs & methods added

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,27 @@
-  SYMBOLS = {
-    heart => ["♥", :red],
-    diamond => ["♦", :red],
-    spade => ["♠", :black],
-    club => ["♣", :black]
-  }
+  class Card
+    attr_reader :suit, :value, :color, :symbol
+
+    SYMBOLS = {
+      "heart" => ["♥", :red],
+      "diamond" => ["♦", :red],
+      "spade" => ["♠", :black],
+      "club" => ["♣", :black]
+    }
+
+    VALUES = {
+      11 => "J",
+      12 => "Q",
+      13 => "K",
+      14 => "A"
+    }
+
+    def initialize(suit, value)
+      raise ArgumentError.new("Invalid suit") unless SYMBOLS.keys.include?(suit)
+      raise ArgumentError.new("Invalid value") unless (1..14).include?(value)
+
+      @suit = suit
+      @value = value > 10 ? VALUES[value] : value
+      @symbol = SYMBOLS[suit][0]
+      @color = SYMBOLS[suit][1]
+    end
+  end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,8 +5,37 @@ describe Card do
 
 
   describe '#initialize' do
-    it 'takes suit as an attribute'
+    let(:card) { Card.new("spade", 10)}
+    let(:face_card) { Card.new("spade", 12)}
+    let(:bad_symbol) { Card.new("bogus", 10) }
+    let(:bad_value) { Card.new("spade", 20) }
 
-    it 'takes value as an attribute'
+    it 'takes suit as an attribute' do
+      expect(card.suit).to eq("spade")
+    end
+
+    it 'takes value as an attribute' do
+      expect(card.value).to eq(10)
+    end
+
+    it 'sets value to face card name if above 10' do
+      expect(face_card.value).to eq("Q")
+    end
+
+    it 'sets color based on suit' do
+      expect(card.color).to eq(:black)
+    end
+
+    it 'sets symbol based on suit' do
+      expect(card.symbol).to eq("♠")
+    end
+
+    it 'raises error if symbol not recognized' do
+      expect{ bad_symbol }.to raise_error(ArgumentError)
+    end
+
+    it 'raises error if value is incorrect' do
+      expect { bad_value }.to raise_error(ArgumentError)
+    end
   end
 end
