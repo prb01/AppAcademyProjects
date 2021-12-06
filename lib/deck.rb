37c763d44d4d2f2eb94f4740ebc9ab442dc4c800
prb01 <p.bergstroem@gmail.com>
2021-12-06 08:58:29 +0200

    #take_card, #add_card, #valid_card? methods added with specs

--- lib/deck.rb
+++ lib/deck.rb
@@ -15,4 +15,23 @@ class Deck
   def shuffle!
     @cards.shuffle!
   end
+
+  def take_card
+    @cards.pop
+  end
+
+  def add_card(card)
+    raise ArgumentError.new("Card is not valid") unless valid_card?(card)
+    @cards.unshift(card)
+  end
+
+  private
+  def valid_card?(card)
+    if card.is_a?(Card) &&
+      ( self.cards.none? { |c| c.value == card.value && c.suit == card.suit })
+      return true
+    else
+      return false
+    end
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -33,4 +33,38 @@ describe Deck do
       expect(cards).not_to eq(shuffled_cards)
     end
   end
+
+  describe '#take_card' do
+    it 'returns the taken card' do
+      last_card = deck.cards[-1]
+      expect(deck.take_card).to eq(last_card)
+    end
+
+    it 'reduces deck by card taken' do
+      deck.take_card
+      expect(deck.cards.count).to eq(51)
+    end
+  end
+
+  describe '#add_card' do
+    let(:taken_card) { deck.take_card }
+    let(:card) { double(Card, :value => 15, :suit => "club") }
+    
+    it 'adds card to bottom of pile' do
+      deck.add_card(taken_card)
+      expect(deck.cards[0]).to eq(taken_card)
+    end
+
+    it 'adds to total deck count' do
+      allow(card).to receive(:is_a?).with(Card) { true }
+      deck.take_card
+      expect(deck.cards.count).to eq(51)
+      deck.add_card(card)
+      expect(deck.cards.count).to eq(52)
+    end
+
+    it 'only allows card of class Card to be added' do
+      expect{ deck.add_card("card") }.to raise_error(ArgumentError)
+    end
+  end
 end
