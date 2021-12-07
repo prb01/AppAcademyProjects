4de550a1a95e64ef28bd694807e4ea945916daf6
prb01 <p.bergstroem@gmail.com>
2021-12-07 15:26:12 +0200

    Game specs updated

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require 'game'
 
 describe Game do
-  
+  let(:game) { Game.new(("A".."D").to_a, 100, 5) }
+
+  describe '#initialize' do
+    it 'takes names array as an input to create players' do
+      expect(game.players.length).to eq(4)
+    end
+  end
 end
