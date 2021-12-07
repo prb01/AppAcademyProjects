32b330d02444df627cda90fd06d3d4ea57e09edc
prb01 <p.bergstroem@gmail.com>
2021-12-07 09:29:00 +0200

    Initial commit for class Player & specs

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+require 'rpsec'
+require 'player'
+
+describe Player do
+  let(:player) { Player.new("PRB", 100) }
+
+  describe '#initialize' do
+    it 'takes name as an input' do
+      expect()
+    end
+
+    it 'takes pot as an input'
+    it 'initializes hand as hand class'
+  end
+
+  describe '#change_pot' do
+  end
+
+  describe '#discard' do
+  end
+end
