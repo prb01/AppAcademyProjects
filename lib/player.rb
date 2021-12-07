1fb512551c3edd1b166400ffe716acd1e4eb1599
prb01 <p.bergstroem@gmail.com>
2021-12-07 09:41:13 +0200

    Player#initialize & #change_pot methods & specs added

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,19 @@
+require_relative 'hand'
+
 class Player
+  attr_reader :name, :pot, :hand
+
+  def initialize(name, pot = 100)
+    @name = name
+    @pot = pot
+    @hand = Hand.new
+  end
+
+  def change_pot(amt)
+    new_pot = @pot + amt
+    raise ArgumentError.new("Not enough in the pot.") if new_pot < 0
+    @pot = new_pot
+  end
+
+  
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,19 +1,44 @@
-require 'rpsec'
+require 'rspec'
 require 'player'
 
 describe Player do
   let(:player) { Player.new("PRB", 100) }
+  let(:hand) { double(Hand) }
 
   describe '#initialize' do
     it 'takes name as an input' do
-      expect()
+      expect(player.name).to eq("PRB")
     end
 
-    it 'takes pot as an input'
-    it 'initializes hand as hand class'
+    it 'takes pot as an input' do
+      expect(player.pot).to eq(100)
+    end
+
+    it 'initializes hand as hand class' do
+      expect(player.hand).to be_a(Hand)
+    end
   end
 
   describe '#change_pot' do
+    context 'when input is positive' do
+      it 'adds amount to player pot' do
+        expect(player.pot).to eq(100)
+        player.change_pot(50)
+        expect(player.pot).to eq(150)
+      end
+    end
+
+    context 'when input is negative' do
+      it 'subtracts amount from player pot' do
+        expect(player.pot).to eq(100)
+        player.change_pot(-50)
+        expect(player.pot).to eq(50)
+      end
+
+      it 'raises error if amount is more than player pot' do
+        expect{ player.change_pot(-150) }.to raise_error(ArgumentError)
+      end
+    end
   end
 
   describe '#discard' do
