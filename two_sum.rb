777f8b72ad6607b4a6e0fdf8e3540544dfba1ca7
prb01 <p.bergstroem@gmail.com>
2021-12-10 11:59:27 +0200

    Two Sum exercise

--- two_sum.rb
+++ two_sum.rb
@@ -22,47 +22,43 @@ def okay_two_sum?(arr, target)
   left = 0
   right = sorted.length - 1
 
-  while left <= right
-    mid = ((left + right) / 2).floor
-
-    if sorted[mid] + sorted[mid-1] < target
-      left = mid + 1
-    elsif sorted[mid] + sorted[mid-1] > target
-      right = mid - 1
-    elsif sorted[mid] + sorted[mid-1] == target
+  while left < right
+    case (arr[left] + arr[right] <=> target)
+    when 0
       return true
+    when -1
+      left += 1
+    when 1
+      right -= 1
     end
   end
 
   false
 end
 
-arr = [0, 1, 5, 7]
+# arr = [0, 1, 5, 7]
 # p okay_two_sum?(arr, 6) # => should be true
 # p okay_two_sum?(arr, 10) # => should be false
-arr = [0, 2, 10, 4, 8]
+# arr = [0, 2, 10, 4, 8]
 # p okay_two_sum?(arr, 6) # => should be true
 # p okay_two_sum?(arr, 10) # => should be true
 
 
 #Time:  O(n)
-#Space: O(1)
+#Space: O(n)
 def two_sum?(arr, target)
   hash = Hash.new
   arr.each do |num|
-    hash[num] = (num <= target)
-  end
-  
-  (0..(target/2.0).ceil-1).each do |i|
-    return true if hash[i] && hash[target - i]
+    return true if hash[target - num]
+    hash[num] = true
   end
 
   false
 end
 
-arr = [0, 1, 5, 7]
-p two_sum?(arr, 6) # => should be true
-p two_sum?(arr, 10) # => should be false
-arr = [0, 2, 10, 4, 8]
-p two_sum?(arr, 6) # => should be true
-p two_sum?(arr, 10) # => should be true
+# arr = [0, 1, 5, 7]
+# p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10) # => should be false
+# arr = [0, 2, 10, 4, 8]
+# p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10) # => should be true
