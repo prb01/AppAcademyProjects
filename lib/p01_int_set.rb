e8af2f0362171a10f5ee42b393edb54800595cd2
prb01 <p.bergstroem@gmail.com>
2021-12-14 08:29:37 +0100

    Initial commit for Hashmap&LRUCache classwork

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,90 @@
+class MaxIntSet
+  attr_reader :store
+  
+  def initialize(max)
+    @max = max
+    @store = Array.new(max + 1, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num] == true
+  end
+
+  private
+
+  def is_valid?(num)
+    num <= @max && num >= 0
+  end
+
+  def validate!(num)
+    raise ArgumentError.new("Out of bounds") unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
