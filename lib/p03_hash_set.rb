cbf05918628d35824a7bc0ec621a4124bf400434
prb01 <p.bergstroem@gmail.com>
2021-12-14 10:08:10 +0100

    HashSet class added

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    unless self.include?(key)
+      self[key] << key
+      @count += 1
+    end
+
+    resize! unless num_buckets > count
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key) 
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    mod = key.hash % num_buckets
+    @store[mod]
   end
 
   def num_buckets
@@ -26,5 +39,19 @@ class HashSet
   end
 
   def resize!
+    num_buckets.times { @store << Array.new }
+
+    store.each_with_index do |subarr, i|
+      break if i >= num_buckets / 2
+
+      temp_arr = []
+      until subarr.empty?
+        temp_arr << subarr.pop
+      end
+
+      temp_arr.each do |key|
+        self[key] << key
+      end
+    end
   end
 end
