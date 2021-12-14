c064fdcbc2170c5f38304bfbff15846f62f3ec6c
prb01 <p.bergstroem@gmail.com>
2021-12-14 10:00:27 +0100

    Array, String, Hash #hash functions added

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    return 0.to_s(8).to_i if self.empty?
+
+    ln = self.length
+    xor_num = self.first.to_s.unpack("c*").sum
+
+    (0...ln).each do |i|
+      xor_num = xor_num ^ (self[i].to_s.unpack("c*").sum * i)
+    end
+
+    xor_num.hash
   end
 end
 
 class String
   def hash
+    self.unpack("c*").hash
   end
 end
 
@@ -16,6 +27,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.hash
   end
 end
