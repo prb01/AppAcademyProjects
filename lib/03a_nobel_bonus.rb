c19668c9510fc4ce2c9b579d4483dbaf94f3d71f
prb01 <p.bergstroem@gmail.com>
2021-12-16 10:07:12 +0100

    03a_nobel_bonus specs passed

--- lib/03a_nobel_bonus.rb
+++ lib/03a_nobel_bonus.rb
@@ -11,5 +11,14 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+  SELECT
+    DISTINCT n1.yr
+  FROM
+    nobels n1
+  LEFT JOIN
+    nobels n2 ON (n1.yr = n2.yr AND n2.subject = 'Chemistry')
+  WHERE
+    n2.subject IS NULL AND
+    n1.subject = 'Physics'
   SQL
 end
