fe29b05a872148b82eb2ee3c93c97a01f227fab0
prb01 <p.bergstroem@gmail.com>
2021-12-29 10:06:07 +0100

    Associations added between Course, Enrollment, User

--- associations/app/models/enrollment.rb
+++ associations/app/models/enrollment.rb
@@ -1,2 +1,14 @@
 class Enrollment < ApplicationRecord
+  :user
+  :course
+
+  belongs_to :user,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :student_id
+
+  belongs_to :course,
+    class_name: 'Course',
+    primary_key: :id,
+    foreign_key: :course_id
 end
--- associations/app/models/user.rb
+++ associations/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+    class_name: 'Enrollment',
+    primary_key: :id,
+    foreign_key: :student_id
+  
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
+
+# Add associations for enrollments and enrolled_courses. This might take a little 
+# bit of thinking.
+
+# You will know you have succeeded when you can execute User.first.enrollments and 
+# User.first.enrolled_courses in the rails console. These commands should 
+# return the user's enrollments and enrolled courses.
