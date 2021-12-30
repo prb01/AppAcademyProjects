5d5fded06bb96504287ba48d3f1ab6f73a1aa6d7
prb01 <p.bergstroem@gmail.com>
2021-12-30 11:11:32 +0100

    Visit association refactor. TagTopic#popular_links added
     refactor: Visit associations to use belongs_to instead

--- URLShortener/app/models/tag_topic.rb
+++ URLShortener/app/models/tag_topic.rb
@@ -9,4 +9,14 @@ class TagTopic < ApplicationRecord
   has_many :urls,
     through: :taggings,
     source: :url
+
+  has_many :visits,
+    through: :urls,
+    source: :visits
+
+  def popular_links
+    self.visits.select(:url_id).group(:url_id).limit(5).count.to_a.map do |arr|
+      [ShortenedUrl.find_by_id(arr[0]), arr[1]]
+    end
+  end
 end
--- URLShortener/app/models/visit.rb
+++ URLShortener/app/models/visit.rb
@@ -1,15 +1,15 @@
 class Visit < ApplicationRecord
   validates :url_id, :user_id, presence: true
 
-  has_many :url,
+  belongs_to :url,
     class_name: 'ShortenedUrl',
-    primary_key: :url_id,
-    foreign_key: :id
+    primary_key: :id,
+    foreign_key: :url_id
 
-  has_many :visitor,
+  belongs_to :visitor,
     class_name: 'User',
-    primary_key: :user_id,
-    foreign_key: :id
+    primary_key: :id,
+    foreign_key: :user_id
   
   def self.record_visit!(user, shortened_url)
     Visit.create(url_id: shortened_url.id, user_id: user.id)
