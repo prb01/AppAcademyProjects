80478f3ede6b0620d8b2001ba344e42d93ddd723
prb01 <p.bergstroem@gmail.com>
2022-01-20 15:09:08 +0100

    User table/model added

--- 06_views_99cats/db/schema.rb
+++ 06_views_99cats/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_080933) do
+ActiveRecord::Schema.define(version: 2022_01_20_135525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2022_01_19_080933) do
     t.datetime "updated_at", null: false
     t.index ["color"], name: "index_cats_on_color"
     t.index ["name"], name: "index_cats_on_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
