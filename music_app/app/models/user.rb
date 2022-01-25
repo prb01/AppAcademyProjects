41cd8ae1c5b7ff0c7a7a474d8b6a267b057c7fbe
prb01 <p.bergstroem@gmail.com>
2022-01-25 11:01:28 +0100

    User refactor: Password allow_nil

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true
   validates :password_digest, presence: { message: "Password can't be blank" }
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_nil: true
   validates :email, :session_token, uniqueness: true
   after_initialize :ensure_session_token
 
