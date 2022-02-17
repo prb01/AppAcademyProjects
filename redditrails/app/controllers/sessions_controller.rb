fe0c4e5ff39c8bf6518e012076f733af751f8d63
prb01 <p.bergstroem@gmail.com>
2022-02-17 11:22:18 -0500

    Users & Sessions controllers added

---
+++ redditrails/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsControlelr < ApplicationController
+  def new
+    render :new
+  end
+  
+  def create
+    user = User.find_by_credentials(user_params[:user][:email], user_params[:user][:password])
+  
+    if user
+      flash[:notify] = "#{self.email} account successfully logged in"
+      login!(user)
+      redirect_to subs_url
+    else
+      flash.now[:errors] = "Email/Password combo not valid"
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+end
---
+++ redditrails/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    render :new
+  end
+  
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      flash[:notify] = "#{self.email} account successfully created"
+      login!(user)
+      redirect_to subs_url
+    else
+      flash.now[:errors] = user.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    user = current_user
+    logout!
+    user.destroy
+    redirect_to subs_url
+  end
+end
