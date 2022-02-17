2226869fbc474bb0f195a2b5fcc8c547950285a1
prb01 <p.bergstroem@gmail.com>
2022-02-17 10:31:29 -0500

    User model added

---
+++ redditrails/app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  include BCrypt
+
+  validates :email, :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :email, :session_token, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def self.generate_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_token
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_token
+    self.save
+    self.session_token
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = Password.create(password)
+  end
+
+  def is_password?(password)
+    Password.new(self.password_digest) == password
+  end
+end
