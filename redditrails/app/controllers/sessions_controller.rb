65860619c4c02e4f9d488276c795835730530d3c
prb01 <p.bergstroem@gmail.com>
2022-02-18 18:25:52 -0500

    User forms added with errors & notification templates

--- redditrails/app/controllers/sessions_controller.rb
+++ redditrails/app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
-class SessionsControlelr < ApplicationController
+class SessionsController < ApplicationController
   def new
     render :new
   end
   
   def create
-    user = User.find_by_credentials(user_params[:user][:email], user_params[:user][:password])
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
   
-    if user
-      flash[:notify] = "#{self.email} account successfully logged in"
-      login!(user)
-      redirect_to subs_url
+    if @user
+      flash[:notify] = ["#{@user.email} account successfully logged in"]
+      login!(@user)
+      redirect_to new_session_url
+      # redirect_to subs_url
     else
-      flash.now[:errors] = "Email/Password combo not valid"
+      flash.now[:errors] = ["Email/Password combo not valid"]
       render :new
     end
   end
--- redditrails/app/controllers/users_controller.rb
+++ redditrails/app/controllers/users_controller.rb
@@ -4,22 +4,23 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      flash[:notify] = "#{self.email} account successfully created"
-      login!(user)
-      redirect_to subs_url
+    if @user.save
+      flash[:notify] = ["#{@user.email} account successfully created"]
+      login!(@user)
+      redirect_to new_user_url
+      # redirect_to subs_url
     else
-      flash.now[:errors] = user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
   def destroy
-    user = current_user
+    @user = current_user
     logout!
-    user.destroy
+    @user.destroy
     redirect_to subs_url
   end
 end
